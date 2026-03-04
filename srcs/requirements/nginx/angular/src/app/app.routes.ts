8e8d876f2d9b01c10155c5fa9adc3c96740158a2
aabouyaz <aabouyaz@vme.myguest.virtualbox.org>
2026-03-04 16:47:02 +0100

    Docker Image Nginx: Copie simple du build angular

--- srcs/requirements/nginx/angular/src/app/app.routes.ts
+++ srcs/requirements/nginx/angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Game } from './pages/game/game';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '',  component: Game},
+  { path: 'game',  component: Game},
+  { path: 'test',  component: Home},
+  { path: 'home', component: Home}
+];
