fa2696311deddc3ca9040873c2f47d08bfcb1112
Elena <elena.duranceau>
2026-03-05 14:31:12 +0100

    error page avec code dynamique et citations

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { ErrorPageComponent } from './error-page/error-page';
 
 export const routes: Routes = [
   { path: 'game', component: GameComponent },
-  { path: '', redirectTo: 'game', pathMatch: 'full' },
+  { path: '404', component: ErrorPageComponent, data: { code: 404 } },
+  { path: '403', component: ErrorPageComponent, data: { code: 403 } },
+  { path: '500', component: ErrorPageComponent, data: { code: 500 } },
+  { path: '**', redirectTo: '/404' },
 ];
