15624c158eaac02bb956772818f577458cda7932
Anas Abouyazid <aabouyaz@student.42paris.fr>
2026-03-05 18:03:48 +0100

    debut de login (front et back lies)

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { Home } from './home/home';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: 'game', component: GameComponent },
-  { path: '', redirectTo: 'game', pathMatch: 'full' },
+  { path: '', component: Home },
+  { path: 'game', component: GameComponent }, 
+  { path: '**', component: NotFoundComponent },
+ 
 ];
---
+++ angular/src/app/home/logo/logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss',
+})
+export class LogoComponent {}
