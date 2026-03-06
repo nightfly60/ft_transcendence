e9ba3d48ae60b1672cfae2f326349c384dced927
Gael Mariani <lemarian@f6r5s14.clusters.42paris.fr>
2026-03-06 17:13:32 +0100

    modified/commented game.ts to reflect changes in socket management in index.ts, started rework of chat-box angular component to add socket support

--- angular/src/app/chat-box/chat-box.ts
+++ angular/src/app/chat-box/chat-box.ts
@@ -1,6 +1,10 @@
-import { Component, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewChecked, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { SocketService } from '../services/socket.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 interface Message {
   text: string;
@@ -15,7 +19,8 @@ interface Message {
   templateUrl: './chat-box.html',
   styleUrl: './chat-box.scss',
 })
-export class ChatBox implements AfterViewChecked {
+
+export class ChatBox implements AfterViewChecked { //pretty sure this does nothing...
   @ViewChild('messagesContainer') private messagesContainer!: ElementRef;
   
     inputText = '';
@@ -31,18 +36,18 @@ export class ChatBox implements AfterViewChecked {
       this.scrollToBottom();
     }
   
-    // sendMessage(): void {
-    //   const text = this.inputText.trim();
-    //   if (!text) return;
+    sendMessage(): void {
+      const text = this.inputText.trim();
+      if (!text) return;
   
-    //   this.messages.push({
-    //     text,
-    //     type: 'outgoing',
-    //     timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-    //   });
+      this.messages.push({
+        text,
+        type: 'outgoing',
+        timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      });
   
-    //   this.inputText = '';
-    // }
+      this.inputText = '';
+    }
   
     private scrollToBottom(): void {
       try {
@@ -50,4 +55,14 @@ export class ChatBox implements AfterViewChecked {
           this.messagesContainer.nativeElement.scrollHeight;
       } catch (e) {}
     }
+}
+
+export class NewChatBox implements OnInit {
+  gameId = 'partie-1'; //placeholder
+  user$!: Observable<User>;
+  
+  constructor(private socket: SocketService, private http: HttpClient) {} // ?
+  ngOnInit() {
+    
+  }
 }
--- nodejs/src/server/sockets/game.ts
+++ nodejs/src/server/sockets/game.ts
@@ -1,15 +1,15 @@
 import { Server, Socket } from 'socket.io';
 
 export function registerGameEvents(io: Server, socket: Socket): void {
-  console.log(`Player connected: ${socket.id}`);
+  console.log(`Player connected: ${socket.id}`); //replace with game start?
 
-  socket.on('join_game', (gameId: string) => {
-    socket.join(gameId);
-    console.log(`${socket.id} joined game ${gameId}`);
-  });
+  // socket.on('join_game', (gameId: string) => { //
+  //   socket.join(gameId);
+  //   console.log(`${socket.id} joined game ${gameId}`);
+  // });
 
-  socket.on('move', (data: { gameId: string; from: string; to: string }) => {
-    socket.to(data.gameId).emit('move', { from: data.from, to: data.to });
+  socket.on('move', (data: { from: string; to: string }) => {
+    socket.to(socket.data.gameId).emit('move', { from: data.from, to: data.to });
   });
 
   socket.on('disconnect', () => {
