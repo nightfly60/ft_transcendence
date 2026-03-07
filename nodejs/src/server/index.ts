ab79e88653f9d78c6279a55fcdce96cd30aaabfc
Elena <elena.duranceau>
2026-03-07 16:47:44 +0100

    modif profil: username, bio, upload image

--- nodejs/src/server/index.ts
+++ nodejs/src/server/index.ts
@@ -9,6 +9,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRouter from './routes/auth.routes';
 import { requireAuth } from './middleware/auth.middleware.js';
+import profileEditRouter from './routes/profile-edit.routes.js'
 
 const app = express();
 const httpServer = createServer(app);
@@ -33,7 +34,8 @@ app.use('/avatars', (req, res, next) => {
 console.log('Serving avatars from:', path.join(__dirname, 'public/avatars'));
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.get('/health', (_req, res) => {
   res.json({ status: 'ok' });
@@ -41,6 +43,7 @@ app.get('/health', (_req, res) => {
 
 app.use('/users', requireAuth, userRouter);
 app.use('/profile', requireAuth, profileRouter); 
+app.use('/profile-edit', profileEditRouter);
 
 io.on('connection', (socket) => {
   registerGameEvents(io, socket);
