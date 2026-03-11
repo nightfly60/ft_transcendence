2164203c8da3a855656900e687bb698ed03e853d
Anas Abouyazid <aabouyaz@student.42paris.fr>
2026-03-11 15:23:25 +0100

    Game Statistics and match history

--- mariadb/tools/transcendence.sh
+++ mariadb/tools/transcendence.sh
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS friends (
 CREATE TABLE IF NOT EXISTS User_achievements (
     id_user INT NOT NULL,
     id_achievement INT NOT NULL,
-    type VARCHAR(255),
+    progression VARCHAR(255),
     PRIMARY KEY(id_user, id_achievement),
     FOREIGN KEY(id_user) REFERENCES \`User\`(id),
     FOREIGN KEY(id_achievement) REFERENCES Achievements(id)
