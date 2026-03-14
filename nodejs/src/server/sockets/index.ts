8876ab89122c44b8f30ace5a11b5257abb503d70
Anas Abouyazid <aabouyaz@student.42paris.fr>
2026-03-14 13:49:02 +0100

    Merge Dev 14/03/26

--- nodejs/src/server/sockets/index.ts
+++ nodejs/src/server/sockets/index.ts
@@ -8,7 +8,7 @@ import { registerChatEvents } from './chat.js';
 export function initSockets(httpServer: HttpServer<typeof IncomingMessage, typeof ServerResponse>): void {
   const io = new Server(httpServer, {
     cors: {
-      origin: process.env.ALLOWED_ORIGINS || '*',
+      origin: '*',
       methods: ['GET', 'POST'],
     },
   });
