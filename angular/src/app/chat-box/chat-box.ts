a0d2e47114e186dc2fe3a5e56cfb346d3371470c
Gael Mariani <lemarian@f1br5s3.paris.42.school>
2026-03-17 16:03:49 +0100

    turned game chat into a widget

--- angular/src/app/chat-box/chat-box.ts
+++ angular/src/app/chat-box/chat-box.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked, signal } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, signal, effect } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { SocketService } from '../services/socket.service';
@@ -13,45 +13,54 @@ class Message {
 
 @Component({
   selector: 'app-chat-box',
-  imports: [FormsModule, DatePipe],
+  imports: [FormsModule],
   templateUrl: './chat-box.html',
   styleUrl: './chat-box.scss',
 })
 
-export class ChatBox implements OnInit, AfterViewChecked{
+export class ChatBox implements OnInit{
    @ViewChild('messagesContainer') private messagesContainer!: ElementRef;
   chatID = '';
   conversationId = 0;
   userId = 0; //replace with user object?
   message = '';
   messages = signal<Message[]>([]);
+  panelOpen   = signal(false);
+  
+  private scrollToBottom(): void {
+    if (this.messagesContainer) {
+      this.messagesContainer.nativeElement.scrollTop =
+        this.messagesContainer.nativeElement.scrollHeight;
+    }
+  }
 
-    constructor(private socket: SocketService, private http:HttpClient) {}
+  constructor(private socket: SocketService, private http:HttpClient) {
+    effect(() => {
+      this.messages(); // track signal
+      this.panelOpen(); // track signal
+      setTimeout(() => this.scrollToBottom(), 0);
+    });
+  }
 
-    ngOnInit(): void {
-      this.socket.findChat(); //get chat id from game
-      this.socket.onChatReady(( chatId, userId, conversationId ) => {
-          this.chatID = chatId;
-          this.userId = userId;
-          this.conversationId = conversationId;
-          //check chat history
-          this.http.get<any[]>(`/api/conversation/${this.conversationId}/Message`).subscribe(history => {
-            this.messages.set(history.map(m => new Message(m.content, new Date(m.sent_at), m.id_sender, m.id)));
-          });
-      });
+  ngOnInit(): void {
+    this.socket.findChat(); //get chat id from game
+    this.socket.onChatReady(( chatId, userId, conversationId ) => {
+        this.chatID = chatId;
+        this.userId = userId;
+        this.conversationId = conversationId;
+        //check chat history
+        this.http.get<any[]>(`/api/conversation/${this.conversationId}/Message`).subscribe(history => {
+          this.messages.set(history.map(m => new Message(m.content, new Date(m.sent_at), m.id_sender, m.id)));
+        });
+    });
       
 
-      this.socket.onReceiveMessage(({id, text, senderId, timestamp}) => {
-        const alreadyExists = this.messages().some(m => m.id === id);
-        if (!alreadyExists) {
-          this.messages.update((prev) => [...prev, new Message(text, new Date(timestamp), senderId, id)]);
-        }
-      });
-    }
-
-  
-  ngAfterViewChecked() {
-    this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    this.socket.onReceiveMessage(({id, text, senderId, timestamp}) => {
+      const alreadyExists = this.messages().some(m => m.id === id);
+      if (!alreadyExists) {
+        this.messages.update((prev) => [...prev, new Message(text, new Date(timestamp), senderId, id)]);
+      }
+    });
   }
 
   sendMessage() : void {
@@ -62,4 +71,10 @@ export class ChatBox implements OnInit, AfterViewChecked{
       this.message = '';
     }
   }
+
+  togglePanel(): void {
+    this.panelOpen.update(v => !v);
+  }
+
+  
 }
