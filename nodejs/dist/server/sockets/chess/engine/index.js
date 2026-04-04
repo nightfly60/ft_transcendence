8d11c810162d6fbd98f82c6ec9b26be178e09730
Anas Abouyazid <aabouyaz@student.42paris.fr>
2026-04-04 13:36:55 +0200

    rm dist

--- nodejs/dist/server/sockets/chess/engine/index.js
+++
@@ -1,4 +0,0 @@
-export { initBoard, toAlgebraic, fromAlgebraic } from './board.js';
-export { getRawMoves } from './moves.js';
-export { isKingInCheck, getValidMoves, computeGameStatus } from './validator.js';
-export { applyMove, computeValidMovesMap } from './apply.js';
--- nodejs/dist/server/sockets/chess/types.js
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_CONTEXT = {
-    enPassantTarget: null,
-    castlingRights: { wK: true, wQ: true, bK: true, bQ: true },
-};
